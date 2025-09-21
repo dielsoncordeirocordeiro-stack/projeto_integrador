1edd24cb770e738659edb43aefdafd5f75ad10fa
dalvinha2021 <dalvinha2021@gmail.com>
2025-09-21 14:32:36 +0000

    Mensagem do que foi atualizado

--- pages/index.tsx
+++ pages/index.tsx
@@ -43,17 +43,19 @@ export default function Home() {
     <>
       {/* Google Analytics */}
       <Script
-        strategy="afterInteractive"
-        src="https://www.googletagmanager.com/gtag/js?id=G-KDRM9X7CLC"
-      />
-      <Script id="google-analytics" strategy="afterInteractive">
-        {`
-          window.dataLayer = window.dataLayer || [];
-          function gtag(){dataLayer.push(arguments);}
-          gtag('js', new Date());
-          gtag('config', 'G-KDRM9X7CLC');
-        `}
-      </Script>
+  async
+  src="https://www.googletagmanager.com/gtag/js?id=G-LCX26BWVRF"
+  strategy="afterInteractive"
+/>
+<Script id="google-analytics" strategy="afterInteractive">
+  {`
+    window.dataLayer = window.dataLayer || [];
+    function gtag(){dataLayer.push(arguments);}
+    gtag('js', new Date());
+    gtag('config', 'G-LCX26BWVRF');
+  `}
+</Script>
+
 
       {/* Fonte Playfair Display */}
       <style>{`
